ef464f7e244f61ce434f4cfbcd263c7bd3f364ab
sriya-g <sriya.gottiparthi@gmail.com>
2026-09-22 11:16:08 -0500

    simplified nvim plugins

--- .config/nvim/lua/plugins/organizer.lua
+++ .config/nvim/lua/plugins/organizer.lua
@@ -2,5 +2,6 @@ return {
     "nvim-neorg/neorg",
     lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
     version = "*", -- Pin Neorg to the latest stable release
+    dependencies = { "nvim-lua/plenary.nvim" },
     config = true,
 }
